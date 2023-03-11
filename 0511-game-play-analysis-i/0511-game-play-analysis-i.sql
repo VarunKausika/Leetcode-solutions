3c854c336e2a4f29982fca14c95f1ed5e7bcebd2
Varun Kausika <48988184+VarunKausika@users.noreply.github.com>
2023-03-11 13:25:02 -0600

    Time: 884 ms (70.91%), Space: 0B (100.00%) - LeetHub

---
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+WITH new_table AS (SELECT player_id, event_date, (dense_rank() OVER (PARTITION BY player_id ORDER BY event_date ASC)) AS "first_login" FROM Activity) SELECT player_id, event_date AS first_login FROM new_table WHERE first_login = 1;
